e50745f52c2edb5d42af91f161f3468aef8ff096
Benjamin Tsai <benjamintsa@gmail.com>
2020-02-01 19:13:34 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by Ben Tsai on 2/1/20.
+//  Copyright © 2020 Ben Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
